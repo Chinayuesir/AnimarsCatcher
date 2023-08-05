a4d0d67e8bbcdb73d5d52e25e9b88cc87c5238d9
Chinayuesir <37198801+Chinayuesir@users.noreply.github.com>
2023-08-05 11:27:28 +0800

    Chapter9 demo version

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace AnimarsCatcher
 {
+    public enum AniInfoType
+    {
+        Picker,
+        Blaster
+    }
+
     public class UIManager : MonoBehaviour
     {
         public static UIManager Instance { get; private set; }
         private GameModel mGameModel;
+        private AniInfoType mAniInfoType = AniInfoType.Picker;
         
         //Text
         public TextMeshProUGUI Text_Day;
@@ -22,13 +30,24 @@ namespace AnimarsCatcher
         public Button RobotIcon;
         public Button Button_ReturnGame;
         public Button Button_QuitGame;
+        public Button PickerAniIcon;
+        public Button BlasterAniIcon;
         
         //Panel
         public GameObject MenuPanel;
 
+        private Vector3 mBigIconPos;
+        private Vector3 mSmallIconPos;
+        private Vector2 mBigIconSizeDelta;
+        private Vector2 mSmallIconSizeDelta;
+
         private void Awake()
         {
             Instance = this;
+            mBigIconPos = PickerAniIcon.GetComponent<RectTransform>().position;
+            mSmallIconPos = BlasterAniIcon.GetComponent<RectTransform>().position;
+            mBigIconSizeDelta = PickerAniIcon.GetComponent<RectTransform>().sizeDelta;
+            mSmallIconSizeDelta = BlasterAniIcon.GetComponent<RectTransform>().sizeDelta;
         }
 
         public void Init(GameModel gameModel, ReactiveProperty<int> levelTime)
@@ -63,6 +82,21 @@ namespace AnimarsCatcher
                 Debug.Log("Quit Game");
                 Application.Quit();
             });
+            
+            SubscribeAniInfo(gameModel.PickerAniCount,gameModel.InTeamPickerAniCount,
+                AniInfoType.Picker);
+            SubscribeAniInfo(gameModel.BlasterAniCount,gameModel.InTeamBlasterAniCount,
+                AniInfoType.Blaster);
+            
+            PickerAniIcon.onClick.AddListener(() =>
+            {
+                AniIconBtnClick(PickerAniIcon,BlasterAniIcon);
+            });
+            
+            BlasterAniIcon.onClick.AddListener(() =>
+            {
+                AniIconBtnClick(BlasterAniIcon,PickerAniIcon);
+            });
         }
 
         private void Start()
@@ -79,15 +113,50 @@ namespace AnimarsCatcher
             {
                 Text_Crystal.text = count.ToString();
             });
-            mGameModel.InTeamPickerAniCount.Subscribe(count =>
+        }
+
+        private void SubscribeAniInfo(ReactiveProperty<int> sumCount, ReactiveProperty<int> inTeamCount,
+            AniInfoType type)
+        {
+            inTeamCount.Subscribe(count =>
             {
+                if (mAniInfoType != type) return;
                 Text_InTeamAniCount.text = count.ToString();
-                Text_OnGroundAniCount.text = (mGameModel.PickerAniCount.Value - count).ToString();
+                Text_OnGroundAniCount.text = (sumCount.Value - count).ToString();
             });
-            mGameModel.PickerAniCount.Subscribe(count =>
+            sumCount.Subscribe(count =>
             {
-                Text_OnGroundAniCount.text = (count - mGameModel.InTeamPickerAniCount.Value).ToString();
+                if (mAniInfoType != type) return;
+                Text_OnGroundAniCount.text = (count - inTeamCount.Value).ToString();
             });
+        }
+
+        private void AniIconBtnClick(Button button1, Button button2)
+        {
+            switch (mAniInfoType)
+            {
+                case AniInfoType.Picker:
+                    mAniInfoType = AniInfoType.Blaster;
+                    Text_InTeamAniCount.text = mGameModel.InTeamBlasterAniCount.Value.ToString();
+                    Text_OnGroundAniCount.text = (mGameModel.BlasterAniCount.Value -
+                                                  mGameModel.InTeamBlasterAniCount.Value).ToString();
+                    break;
+                case AniInfoType.Blaster:
+                    mAniInfoType = AniInfoType.Picker;
+                    Text_InTeamAniCount.text = mGameModel.InTeamPickerAniCount.Value.ToString();
+                    Text_OnGroundAniCount.text = (mGameModel.PickerAniCount.Value -
+                                                  mGameModel.InTeamPickerAniCount.Value).ToString();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            button1.GetComponent<RectTransform>().DOMove(mSmallIconPos, 0.3f);
+            button1.GetComponent<RectTransform>().DOSizeDelta(mSmallIconSizeDelta, 0.3f);
+            button1.enabled = false;
+            button2.GetComponent<RectTransform>().DOMove(mBigIconPos, 0.3f);
+            button2.GetComponent<RectTransform>().DOSizeDelta(mBigIconSizeDelta, 0.3f);
+            button2.enabled = true;
         }
     }
 }
