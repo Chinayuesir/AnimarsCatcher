75986e18298a717570f867da14aca3252e9a53f7
Iris Fenrir <1741411509@qq.com>
2023-10-08 16:59:59 +0800

    Homework 11-4-1 Network Crystal

---
+++ Assets/Scripts/MultiPlayer/CrystalSpwner.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace AnimarsCatcher
+{
+    public class CrystalSpwner : NetworkBehaviour
+    {
+        public GameObject NetCrystalPrefab;
+        public int SpawnCount = 5;
+
+        public static int CrystalNum = 0;
+
+        private void Start()
+        {
+            NetworkManager.Singleton.OnServerStarted += () =>
+            {
+                RandomSpawn();
+            };
+        }
+
+        private void Update()
+        {
+            if (CrystalNum == 0) RandomSpawn();
+        }
+
+        public void RandomSpawn()
+        {
+            if (!IsServer) return;
+            for (int i = 0; i < SpawnCount; i++)
+            {
+                GameObject fruit = Instantiate(NetCrystalPrefab,
+                    new Vector3(Random.Range(100, 190),
+                        0, Random.Range(10, 40)), Quaternion.identity);
+                fruit.GetComponent<NetworkObject>().Spawn();
+            }
+
+            CrystalNum += SpawnCount;
+        }
+    }
+}
---
+++ Assets/Scripts/MultiPlayer/NetCrystal.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace AnimarsCatcher
+{
+    public class NetCrystal : NetworkBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsServer)
+            {
+                return;
+            }
+
+            other.SendMessage("ChangeScore", -1, SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+            CrystalSpwner.CrystalNum--;
+        }
+    }
+}
